57144fbdcc7a365896cb22e97f5ee7e7fd29d63e
aerlevsedi <sobasmarta@gmail.com>
2022-03-22 21:29:05 +0100

    X button with jquery

--- Lab2/todoList/demo.js
+++ Lab2/todoList/demo.js
@@ -1,16 +1,17 @@
-"use strict";
+'use strict';
 
 $(document).ready(() => {
-	$("#modal").click(() => {
-		$("#modal").toggle("slow");
-		console.log("Wywołałem się na kliknięcie");
+	$('#modal').click(() => {
+		$('#modal').toggle('slow');
+		console.log('Wywołałem się na kliknięcie');
 	});
 });
 
 $(document).ready(() => {
-	$("#modalRemove").click(() => {
-		$("#modalRemove").toggle("slow");
-		console.log("Wywołałem się na kliknięcie");
+	$('#modalRemove').click(() => {
+		$('#modalRemove').toggle('slow');
+		console.log('Wywołałem się na kliknięcie');
+		return 1;
 	});
 });
 
@@ -19,67 +20,43 @@ const test = (n) => {
 };
 
 const addNewTask = () => {
-	const newTask = document.getElementById("new-task").value;
+	const newTask = document.getElementById('new-task').value;
 
-	if (newTask === "") {
-		console.error("input is empty");
-		$("#modal").toggle("slow");
+	if (newTask === '') {
+		console.error('input is empty');
+		$('#modal').toggle('slow');
 		return;
 	}
 
 	console.log(`Task: ${newTask}`);
 
-	const listOfTasks = document.getElementById("tasks-list");
+	const listOfTasks = document.getElementById('tasks-list');
 
-	const newLI = document.createElement("li");
-	newLI.id = "listElem";
-	newLI.style.backgroundColor = "white";
-	newLI.className = "list-group-item";
-	newLI.style.width = "500px";
+	const newLI = document.createElement('li');
+	newLI.id = 'listElem';
+	newLI.className = 'list-group-item tasks-list';
+	newLI.style.backgroundColor = 'white';
+	newLI.style.textAlign = 'left';
+	newLI.style.width = '500px';
 
-	function removeTask() {
-		listOfTasks.removeChild(newLI);
-	}
+	$(newLI).append(
+		$(document.createElement('newButton'))
+			.prop({
+				type: 'button',
+				id: 'rmButtonID',
+				innerHTML: 'X',
+				class: 'btn btn-default',
+			})
+			.click(function () {
+				console.log('Hello');
+				$(newLI).remove();
+				$('#modalRemove').toggle('slow');
+			})
+	);
 
-	const newButton = document.createElement("button");
-	newButton.type = "button";
-	newButton.className = "btn btn-default";
-	newButton.innerHTML = "X";
-	newButton.onclick = function () {
-		$("#modalRemove").toggle("slow");
-		listOfTasks.removeChild(newLI);
-	};
-	newLI.appendChild(newButton);
-
-	// $(document).ready(function () {
-	// 	$(newLI).append(
-	// 		$(document.createElement("rmButton")).prop({
-	// 			type: "button",
-	// 			innerHTML: "Press me",
-	// 			class: "btn-styled",
-	// 			onclick: function () {
-	// 				console.log("Hello");
-	// 				$("#modalRemove").toggle("slow");
-	// 			},
-	// 		})
-	// 	);
-	// });
-
-	// $(newLI).append(
-	// 	$(document.createElement("rmButton")).prop({
-	// 		type: "button",
-	// 		innerHTML: "X",
-	// 		class: "btn btn-default",
-	// 		onclick: function () {
-	// 			console.log("Hello");
-	// 			$("#modalRemove").toggle("slow");
-	// 		},
-	// 	})
-	// );
-
-	const newP_Task = document.createElement("div");
+	const newP_Task = document.createElement('div');
 	newP_Task.innerHTML = ` ${newTask}`;
-	newP_Task.style.display = "inline";
+	newP_Task.style.display = 'inline';
 
 	newLI.onclick = function () {
 		taskClicked(newLI);
@@ -91,22 +68,22 @@ const addNewTask = () => {
 
 const taskClicked = (element) => {
 	const task = element.children[1];
-	task.style.display = "inline";
+	task.style.display = 'inline';
 
 	console.log(`Kupa: ${element.innerHTML}`);
 
-	if (element.style.backgroundColor === "grey") {
-		element.style.backgroundColor = "white";
-		task.style.textDecoration = "none";
+	if (element.style.backgroundColor === 'grey') {
+		element.style.backgroundColor = 'white';
+		task.style.textDecoration = 'none';
 
 		if (element.hasChildNodes()) {
 			element.removeChild(element.children[2]);
 		}
 	} else {
-		element.style.backgroundColor = "grey";
-		task.style.textDecoration = "line-through";
+		element.style.backgroundColor = 'grey';
+		task.style.textDecoration = 'line-through';
 
-		const newP_Date = document.createElement("p");
+		const newP_Date = document.createElement('p');
 		const date = new Date();
 
 		console.log(date.toLocaleDateString());
@@ -114,7 +91,7 @@ const taskClicked = (element) => {
 		// check if minutes and hours < 10
 
 		newP_Date.innerHTML = ` [${date.toLocaleDateString()} ${date.getHours()}:${date.getMinutes()}]`;
-		newP_Date.style.display = "inline";
+		newP_Date.style.display = 'inline';
 		// newP_Date.onclick = function () {
 		// 	taskClicked(element);
 		// };
